3c0efa5a60bfd021db190fc8287f9b53e201fee9
ufatfat <lzff@ufatfat.com>
2021-04-28 11:41:42 +0800

    Fix: 修改了路由树

--- router/router.go
+++ router/router.go
@@ -8,33 +8,35 @@ import (
 
 func InitRouter () (r *gin.Engine) {
 	r = gin.Default()
-
-	user := r.Group("/user")
+	api := r.Group("/api")
 	{
-		user.POST("/signin", middleware.IsSignedIn, controller.SignIn)
-		user.Use(middleware.IsSignedOut)
-		user.POST("/signout", controller.SignOut)
-		user.PUT("/password", controller.ChangePassword)
-		user.GET("/myvote", controller.MyVotedWork)
-	}
-	vote := r.Group("/vote")
-	{
-		vote.Use(middleware.IsSignedOut)
-		vote.PUT("", controller.VoteForWork)
-	}
-	work := r.Group("/work")
-	{
-		vote.Use(middleware.IsSignedOut)
-		work.GET("", controller.GetWorkToVote)
-		work.GET("/:workID", func (c *gin.Context) {
-			p := c.Param("workID")
-			switch p {
-			case "num":
-				controller.GetWorkNum(c)
-			default:
-				controller.GetWorkToVoteByID(c)
-			}
-		})
+		user := api.Group("/user")
+		{
+			user.POST("/signin", middleware.IsSignedIn, controller.SignIn)
+			user.Use(middleware.IsSignedOut)
+			user.POST("/signout", controller.SignOut)
+			user.PUT("/password", controller.ChangePassword)
+			user.GET("/myvote", controller.MyVotedWork)
+		}
+		vote := api.Group("/vote")
+		{
+			vote.Use(middleware.IsSignedOut)
+			vote.PUT("", controller.VoteForWork)
+		}
+		work := api.Group("/work")
+		{
+			vote.Use(middleware.IsSignedOut)
+			work.GET("", controller.GetWorkToVote)
+			work.GET("/:workID", func (c *gin.Context) {
+				p := c.Param("workID")
+				switch p {
+				case "num":
+					controller.GetWorkNum(c)
+				default:
+					controller.GetWorkToVoteByID(c)
+				}
+			})
+		}
 	}
 
 	return
